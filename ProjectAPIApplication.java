0ec222fac9cd86ee819b9dfff17418e0ffe24782
NandhiniVP-new <Nandhini.V.P@ibm.com>
2024-09-05 17:37:13 +0530

    Add files via upload

---
+++ ProjectAPIApplication.java
@@ -0,0 +1,11 @@
+package Activities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAPIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAPIApplication.class, args);
+    }
+}
